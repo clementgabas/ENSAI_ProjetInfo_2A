2a1f0a2c18d83d8f51c4b6f64fface71dd89936d
Zacharie Bouhin <zachb974@gmail.com>
2020-10-24 19:35:15 +0200

    Test flask

---
+++ FLASK/Testprojflask/App/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('Pages/home.html')
+
+@app.route('/friends')
+def friends():
+    return render_template('Pages/friends.html')
+
+if __name__ == '__main__':
+    app.run (debug=True, port = 999)
