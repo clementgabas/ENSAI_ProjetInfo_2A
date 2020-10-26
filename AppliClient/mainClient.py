8d82db0abcbafc0b55adabef0dbe746616986b2c
Clement GABAS <clementgabascg65@gmail.com>
2020-10-26 11:28:09 +0100

    Insertion du menu principal

--- AppliClient/mainClient.py
+++ AppliClient/mainClient.py
@@ -1,5 +1,8 @@
 #Main du client
 import socket
+from View.menu_principal import Menu_Principal
+
+
 
 #Quand on lance l'appli client, on se connecte d'abord au serveur.
 #Nous avonc choisi d'héberger le serveur en local (IP = 127.0.0.1) sur le port 5566 qui est libre.
@@ -11,8 +14,11 @@ try:
     sock.connect((HOST, PORT))
     print("Client connecté") #a ce moment la, le client est bien connecté au serveur de l'application.
     #On peut maintenant gérer l'application et permettre au clien de naviguer dedans
-
     #on envoit le client sur le menu principal de l'application
+
+    MenuP1 = Menu_Principal()
+    MenuP1.make_choice()
+
 
     data = "Données qu'on envoit au serveur"
     data = data.encode("utf8")
