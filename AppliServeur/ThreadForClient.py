ba67a988a73a35a55cc520031cf22618adf92e3c
Clement GABAS <clementgabascg65@gmail.com>
2020-10-26 11:11:20 +0100

    ThreadForClient en class à part

---
+++ AppliServeur/ThreadForClient.py
@@ -0,0 +1,15 @@
+#Classe ThreadForClient qui permet la gestion de plusieurs clients en simultanné pr le serveur via les threads.
+
+import threading
+
+
+class ThreadForClient(threading.Thread):
+    def __init__(self, conn):
+        threading.Thread.__init__(self)
+        self.conn = conn
+
+    def run(self):
+        data = self.conn.recv(1024)
+        data = data.decode("utf8")
+        print(data)
+
--- AppliServeur/mainServeur.py
+++ AppliServeur/mainServeur.py
@@ -1,18 +1,7 @@
 #Main du serveur
 import socket
 import threading
-
-class ThreadForClient(threading.Thread):
-    def __init__(self, conn):
-        threading.Thread.__init__(self)
-        self.conn = conn
-
-    def run(self):
-        data = self.conn.recv(1024)
-        data = data.decode("utf8")
-        print(data)
-
-
+from ThreadForClient import ThreadForClient
 
 HOST, PORT = ('', 5566)
 
