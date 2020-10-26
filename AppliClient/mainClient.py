e75d4526fcae03832ae7d1c522cb137ebb90d8f8
Clement GABAS <clementgabascg65@gmail.com>
2020-10-26 11:05:47 +0100

    Premieres versions des mains client et serveur. Création du serveur en local. Gestion de plusieurs clients en simultané via les Threads

--- AppliClient/mainClient.py
+++ AppliClient/mainClient.py
@@ -1,1 +1,19 @@
 #Main du client
+import socket
+
+HOST, PORT = ('localhost', 5566) #pour HOST, l'IP du local est 127.0.0.1
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #init du socket
+
+try:
+    sock.connect((HOST, PORT))
+    print("Client connecté")
+
+    data = "Données qu'on envoit au serveur"
+    data = data.encode("utf8")
+    sock.sendall(data)
+
+except:
+    print("Connexion au serveur échouée.")
+finally:
+    sock.close()
--- AppliServeur/mainServeur.py
+++ AppliServeur/mainServeur.py
@@ -1,1 +1,32 @@
 #Main du serveur
+import socket
+import threading
+
+class ThreadForClient(threading.Thread):
+    def __init__(self, conn):
+        threading.Thread.__init__(self)
+        self.conn = conn
+
+    def run(self):
+        data = self.conn.recv(1024)
+        data = data.decode("utf8")
+        print(data)
+
+
+
+HOST, PORT = ('', 5566)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #init du socket
+sock.bind((HOST, PORT))
+print("Le serveur est démarré.")
+
+while True:
+    sock.listen()
+    conn, address = sock.accept()
+    print("En écoute...")
+
+    my_thread = ThreadForClient(conn)
+    my_thread.start()
+
+conn.close() #on ferme la co à la fin quand on en a plus besoin
+sock.close() #on ferme le socket à la fin quand on en a plus besoin
