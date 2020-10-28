e7a500c33ea3fae97fb95ecc1a97796e0a9f780b
Clement GABAS <clementgabascg65@gmail.com>
2020-10-28 18:02:54 +0100

    maj

--- AppClient2/menu_Creer_Compte.py
+++ AppClient2/menu_Creer_Compte.py
@@ -27,16 +27,61 @@ class Menu_Creer_Compte(AbstractView):
                 'message': "Veuillez confirmer votre mot de passe",
             }
         ]
+        self.questionsPseudo = [
+            {
+                'type': 'input',
+                'name': 'pseudo',
+                'message': "Veuillez insérer votre pseudo",
+            },]
         while True:
             self.reponse = inquirer.prompt(self.questions)
             if self.reponse["Password"] != self.reponse["password_Check"]:
-                print("Les mot de passes ne correspondent pas. Veuillez réessayer")
+                print("Les mot de passes ne correspondent pas.")
+                return self.make_choice_retour()
+                
+            #on simule l'API.
+            #elle prend en entrée l'id et le mdp et elle vérifie si c'est bon.
+            is_id_free = True
+            id_mdp_legal = True
+            has_API_worked = True
+
+            #ensuite elle demande un pseudo et vérifie si il est libre
+            self.reponsePseudo = inquirer.prompt(self.questionsPseudo)
+            pseudo = self.reponsePseudo['pseudo']
+
+            is_pseudo_legit = True
+            is_pseudo_free = True
+
+            print("Compte créer avec succès. Veuillez vous authentifiez svp")
+            import menu_Connexion as MC
+            Co = MC.Menu_Connexion()
+            Co.display_info()
+            return Co.make_choice()
+
+    def make_choice_retour(self):
+        self.questions_retour = [
+            {
+                'type': 'list',
+                'name': 'Retour',
+                'message': "Que souhaitez-vous faire ?",
+                    'choices': [
+                        'Réessayer',
+                        'Retourner à l\'accueil',
+                    ]
+            },
+        ]
+        while True:
+            self.reponse_retour = inquirer.prompt(self.questions_retour)
+            if self.reponse_retour['Retour'] == "Réessayer":
+                return self.make_choice()
+            elif self.reponse_retour['Retour'] == "Retourner à l'accueil":
+                import menu_Accueil as MA
+                Retour = MA.Menu_Accueil()
+                Retour.display_info()
+                return Retour.make_choice()
             else:
-                print("Compte créer avec succès, vous allez être redirigé vers le menu principal")
-                break
-
-
-
+                print("Erreur dans menu_Creer_Comtpe.Menu_CreerCompte.make_choice_retour")
+            break
 
 if __name__ == "__main__": 
     menu_Creer_Compte1  = Menu_Creer_Compte()
