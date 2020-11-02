615269f3e2809f2b91e2c96e4092526a3a20c74a
Clement GABAS <clementgabascg65@gmail.com>
2020-11-02 23:21:53 +0100

    maj du soir

--- AppClient/Travail/appClient.py
+++ AppClient/Travail/appClient.py
@@ -1,6 +1,4 @@
 from Vues.menu_Accueil import Menu_Accueil
-from printFunctions import timePrint as print
-
 
 def lancer_appli():
 	Acceuil = Menu_Accueil()
@@ -8,5 +6,7 @@ def lancer_appli():
 	return Acceuil.make_choice()
 
 if __name__ == "__main__":
-	print("on peut peut etre afficher un dessin via un .txt pour faire genre on 'ouvre' l'appli")
+	with open("Affichagetxt/parchemin_Accueil.txt", "r") as parchemin:
+		aff = parchemin.read()
+		print(aff)
 	lancer_appli()
