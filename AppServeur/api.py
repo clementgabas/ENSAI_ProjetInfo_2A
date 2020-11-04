9652c2d8f1323676b4d9c055c0c3f82a664fc538
Clement GABAS <clementgabascg65@gmail.com>
2020-11-05 00:31:32 +0100

    ajout de print dans l'api

--- AppServeur/api.py
+++ AppServeur/api.py
@@ -79,20 +79,25 @@ def get():
 def new_user():
     request.get_json(force=True)
     username, hpassword, pseudo = request.json.get('username'), request.json.get('hpassword'), request.json.get('pseudo')
+    print(f"Demande de création d'un nouvel utilisateur (username = {username}, pseudo = {pseudo}) dans la base de données.")
 
     #-- on vérifie si un tel username n'existe pas déjà dans la DB
     if DAOuser.does_username_exist(username):
+        print(f"L'identifiant ({username}) existe déjà dans la base de données.")
         response = {"status_code": http_codes.conflict, "message": "User already exists in the DB."} #error 409
         return make_reponse(response, http_codes.conflict)
     #-- on vérifie si un tel pseudo n'existe pas déjà dans la DB
     if DAOuser.does_pseudo_exist(pseudo):
+        print(f"Le pseudo ({pseudo}) existe déjà dans la base de données.")
         response = {"status_code": http_codes.conflict, "message": "Pseudo already exists in the DB."} #error 409
         return make_reponse(response, http_codes.conflict)
 
     #-- on ajoute le nouvel utilisateur (username, pseudo, hpassword) à la DB Utilisateur
     DAOuser.add_user(username, pseudo, hpassword)
+    print("utilisateur ajouté dans la table Utilisateur.")
     #-- on ajoute l'utilisateur à la db Score pour suivre ces classement
     DAOuser.add_user_score(pseudo)
+    print("utilisateur ajouté dans la base Scores.")
     #-- on renvoit le code ok et le message d'ajout de l'utilisateur.
     response = {"status_code": http_codes.ok, "message": "L'utilisateur a bien été ajouté à la DB."}
     return make_reponse(response, http_codes.ok) #code 200
@@ -101,20 +106,24 @@ def new_user():
 def identification():
     request.get_json(force=True)
     username, password= request.json.get('username'), request.json.get('password')
+    print(f"Demande d'identification (identifiant ={username}).")
 
     #-- on vérifie si un utilisateur avec cet identifiant existe dans la DB
     if not DAOuser.does_username_exist(username):
+        print(f"L'identifiant founit ({username}) est incorrect.")
         response = {"status_code": http_codes.unauthorized, "message": "Username incorrect."} #error 401
         return make_reponse(response, http_codes.unauthorized)
 
     #-- on récupère le hpass associé à l'utilisateur et on le compare au hash du mdp fournit pour la connection
     stored_hpass = DAOuser.get_hpass_username(username)
     if not MDPgestion.verify_password(stored_hpass, password):
+        print("Le mot de passe fournit ne correspond pas à celui stocké en base.")
         response = {"status_code": http_codes.unauthorized, "message": "Password incorrect."}  # error 401
         return make_reponse(response, http_codes.unauthorized)
 
     #-- on vérifie que l'utilisateur n'est pas déjà conencté
     if DAOuser.get_est_connecte(username): #l'utilisateur est déjà connecté
+        print(f"L'utilisateur ({username}) est déjà connecté et ne peut donc pas se reconnecter.")
         response = {"status_code": http_codes.forbidden, "message": "User already connected."}  # error 403
         return make_reponse(response, http_codes.forbidden)
 
@@ -123,6 +132,7 @@ def identification():
     DAOuser.update_est_connecte(username, username_or_pseudo = 'username', nouvel_etat = 'True')
     #-- on renvoit le code ok, le message et le pseudo
     pse = DAOuser.get_pseudo(username)
+    print("Utilisateur connecté.")
     response = {"status_code": http_codes.ok, "message": "Connection réussie.", "pseudo": pse}
     return make_reponse(response, http_codes.ok)  # code 200
 
@@ -130,14 +140,17 @@ def identification():
 def deconnect():
     request.get_json(force=True)
     pseudo = request.json.get('pseudo')
+    print(f"Demande de déconenxion (pseudo = {pseudo}).")
 
     # -- on vérifie si un utilisateur avec ce pseudo existe dans la DB
     if not DAOuser.does_pseudo_exist(pseudo):
+        print(f"Le pseudo ({pseudo}) du joueur qui demande sa déconnexion n'existe pas...")
         response = {"status_code": http_codes.unauthorized, "message": "Pseudo incorrect."}  # error 401
         return make_reponse(response, http_codes.unauthorized)
 
     #-- on update le statut "est_connecte" à False de l'utilisateur qui vient de se deco via son pseudo
     DAOuser.update_est_connecte(pseudo, username_or_pseudo = 'pseudo', nouvel_etat = 'False')
+    print(f"Utilisateur (pseudo = {pseudo}) déconnecté")
     #-- on renvoit le code ok et le message.
     response = {"status_code": http_codes.ok, "message": "Déconnection réussie."}
     return make_reponse(response, http_codes.ok)  # code 200
@@ -148,9 +161,11 @@ def deconnect():
 def afficher_liste_amis():
     request.get_json(force=True)
     pseudo = request.json.get('pseudo')
+    print(f"Demande d'affichage de la liste d'amis de pseudo = {pseudo}.")
 
     #-- on récupère la liste des amis
     liste_amis = DAOfriend.afficher_liste_amis(pseudo)
+    print(f"Liste d'amis transmise.")
     #-- on renvoit le code ok, le message et la liste des amis.
     response = {"status_code": http_codes.ok, "message": "Liste des amis récupérée.", 'liste_amis': liste_amis} #code 200
     return make_reponse(response, http_codes.ok)  # code 200
@@ -159,19 +174,23 @@ def afficher_liste_amis():
 def ajout_ami():
     request.get_json(force=True)
     pseudo, pseudo_ami = request.json.get('pseudo'), request.json.get('pseudo_ami')
+    print(f"Demande de pseudo = {pseudo} d'ajouter pseudo = {pseudo_ami} à sa liste d'amis.")
 
     #-- vérification si un utilisateur avec le pseudo_ami existe dans la DB
     if not DAOuser.does_pseudo_exist(pseudo_ami):
+        print(f"Le pseudo ({pseudo_ami}) à ajouter à la liste d'amis n'existe pas.")
         response = {"status_code": http_codes.not_found, "message": "L'ami à ajouter n'existe pas."}  # code 404
         return make_reponse(response, http_codes.not_found)  # code 404
 
     #-- on vérifie si pseudo n'est pas déjà ami avec pseudo_ami
     if DAOfriend.are_pseudos_friends(pseudo, pseudo_ami):
+        print(f"{pseudo} est déjà ami avec {pseudo_ami}.")
         response = {"status_code": http_codes.already_reported, "message": "L'amitié existe déjà."}  # code 208
         return make_reponse(response, http_codes.already_reported)  # code 208
 
     #-- on effectue la procédure qui ajoute le lien d'amitié
     DAOfriend.add_amitie(pseudo, pseudo_ami)
+    print(f"{pseudo_ami} a bien été ajouté à la liste d'ami de {pseudo}.")
     #-- on renvoit le code ok et le message d'ajout de l'ami.
     response = {"status_code": http_codes.ok, "message": "Ami ajouté."}  # code 200
     return make_reponse(response, http_codes.ok)  # code 200
@@ -180,19 +199,23 @@ def ajout_ami():
 def supp_ami():
     request.get_json(force=True)
     pseudo, pseudo_ami = request.json.get('pseudo'), request.json.get('pseudo_ami')
+    print(f"Demande de pseudo = {pseudo} de supprimer pseudo = {pseudo_ami} de sa liste d'amis.")
 
     #-- vérification si un utilisateur avec le pseudo_ami existe dans la DB
     if not DAOuser.does_pseudo_exist(pseudo_ami):
+        print(f"Le pseudo ({pseudo_ami}) à supprimer de la liste d'amis n'existe pas.")
         response = {"status_code": http_codes.not_found, "message": "L'ami à supprimer n'existe pas."}  # code 404
         return make_reponse(response, http_codes.not_found)  # code 404
 
     # -- on vérifie si pseudo est bien ami avec pseudo_ami
     if not DAOfriend.are_pseudos_friends(pseudo, pseudo_ami):
+        print(f"{pseudo} n'est déjà pas ami avec {pseudo_ami}.")
         response = {"status_code": http_codes.already_reported, "message": "L'amitié n'existe pas."}  # code 208
         return make_reponse(response, http_codes.already_reported)  # code 208
 
     # -- on effectue la procédure qui supprime le lien d'amitié
     DAOfriend.sup_amitie(pseudo, pseudo_ami)
+    print(f"{pseudo_ami} a bien été supprimé de la liste d'ami de {pseudo}.")
     #-- on renvoit le code ok et le message de suppression de l'ami.
     response = {"status_code": http_codes.ok, "message": "Ami supprimé."}  # code 200
     return make_reponse(response, http_codes.ok)  # code 200
