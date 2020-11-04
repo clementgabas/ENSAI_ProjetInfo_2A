1b5ae59b2e26ee984ec113e12562dccaeeb06e3d
Clement GABAS <clementgabascg65@gmail.com>
2020-11-04 11:49:36 +0100

    modif db partie

--- AppServeur/database/DataBaseCreation/DBparties.py
+++ AppServeur/database/DataBaseCreation/DBparties.py
@@ -6,8 +6,13 @@ CREATE TABLE "Parties" (
 	"pseudo_proprietaire"	TEXT,
 	"places_total"	INTEGER,
 	"places_dispo"	INTEGER,
-	"J2pseudo" TEXT,
-	"J3pseudo" TEXT,
-	"J4pseudo" TEXT,
-	"J5pseudo" TEXT
+	"J2pseudo"	TEXT,
+	"J3pseudo"	TEXT,
+	"J4pseudo"	TEXT,
+	"J5pseudo"	TEXT,
+	"J1couleur"	TEXT,
+	"J2couleur"	TEXT,
+	"J3couleur"	TEXT,
+	"J4couleur"	TEXT,
+	"J5couleur"	TEXT
 );
