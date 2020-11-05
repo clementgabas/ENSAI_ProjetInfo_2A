7464a59a67cff0114fe0b4801df9f3f74460ce39
Clement GABAS <clementgabascg65@gmail.com>
2020-11-05 14:31:01 +0100

    maj

--- AppClient/Travail/Vues/menu_Salon.py
+++ AppClient/Travail/Vues/menu_Salon.py
@@ -27,26 +27,25 @@ class Salon(AbstractView):
             },
         ]
 
-        def display_info(self):
-            print(f"Vous êtes dans le salon de la salle {self.id_salle}.")
+    def display_info(self):
+        print(f"Vous êtes dans le salon de la salle {self.id_salle}.")
 
-        def make_choice(self):
-            while True:
-                self.reponse = inquirer.prompt(self.questions)
-
-                if self.reponse["Salon_accueil"] == 'Voir les membres de la salle':
-                    pass
-                elif self.reponse["Salon_accueil"] == 'Modifier les paramètres de la salle':
-                    pass
-                elif self.reponse["Salon_accueil"] == "Lancer la partie":
-                    pass
-                else: #'Revenir au menu précédent'
-                    print("Vous allez être redirigés vers le menu précédent.")
-                    import Vues.menu_Choix_Mode_Jeu as MCMJ
-                    Retour = MCMJ.Menu_Choix_Mode_Jeu_Connecte(pseudo=self.pseudo, jeu=self.game)
-                    Retour.display_info()
-                    return Retour.make_choice()
-                break
+    def make_choice(self):
+        while True:
+            self.reponse = inquirer.prompt(self.question)
+            if self.reponse["Salon_accueil"] == 'Voir les membres de la salle':
+                pass
+            elif self.reponse["Salon_accueil"] == 'Modifier les paramètres de la salle':
+                pass
+            elif self.reponse["Salon_accueil"] == "Lancer la partie":
+                pass
+            else: #'Revenir au menu précédent'
+                print("Vous allez être redirigés vers le menu précédent.")
+                import Vues.menu_Choix_Mode_Jeu as MCMJ
+                Retour = MCMJ.Menu_Choix_Mode_Jeu_Connecte(pseudo=self.pseudo, jeu=self.game)
+                Retour.display_info()
+                return Retour.make_choice()
+            break
 
         def voir_membres_salle(self):
             dataPost = {'id_salle': self.id_salle}
