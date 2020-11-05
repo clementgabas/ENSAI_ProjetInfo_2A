9689bab499891ac0a639c1837d093fd0b423f052
Hugo Bernard <hugobernard228@gmail.com>
2020-11-05 14:54:28 +0100

    Creation table parametres et maj de la db

---
+++ AppServeur/database/DataBaseCreation/DBparametres.py
@@ -0,0 +1,8 @@
+CREATE TABLE "Parametres" (
+	"id_parametres"	INTEGER,
+	"jeu"	TEXT,
+	"duree_tour"	INTEGER,
+	"condition_victoire"	INTEGER,
+	"taille_plateau"	TEXT,
+	PRIMARY KEY("id_parametres")
+);
