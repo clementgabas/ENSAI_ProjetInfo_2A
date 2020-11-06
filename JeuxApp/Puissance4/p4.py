7f0f461f4a710e3d18331c179fffabde8f501f6e
RomanePares <romane.pares@gmail.com>
2020-11-06 11:11:16 +0100

    Paramètres P4

--- JeuxApp/Puissance4/p4.py
+++ JeuxApp/Puissance4/p4.py
@@ -6,17 +6,19 @@ Created on Wed Oct 28 09:12:37 2020
 @author: romanepares
 """
 
+
 class Player:
     _name = ""
     _color = ""
     _token = 0
 
-    def __init__(self, name, color):
+
+    def Set_Param(self, name, color):
         self._name = name
         self._color = color
-        if self._color == "Jaune": #Croix A MODIFIER
+        if self._color == "Jaune":  # Croix A MODIFIER
             self._token = 1
-        elif self._color == "Rouge": #Rond A MODIFIER
+        elif self._color == "Rouge":  # Rond A MODIFIER
             self._token = 2
 
 
@@ -28,14 +30,15 @@ class Player:
 class Grid:
     _gridList = []
     _win = 0
-    #RedToken = 1
-    #YellowToken = 2
+
+    # RedToken = 1
+    # YellowToken = 2
 
     def __init__(self, numHeight, numWidth, tokenWinNumber):
         self._numHeight = numHeight
         self._numWidth = numWidth
         self._tokenWinNumber = tokenWinNumber
-        
+
         # init grid a 0
         for k in range(self._numWidth):
             self._gridList.append([0] * self._numHeight)
@@ -43,7 +46,7 @@ class Grid:
 
     def Throw(self, x, tokenColor):
         y = 0
-        
+
         for i in range(self._numHeight):
             if self._gridList[x][i] == 0:
                 self._gridList[x][i] = tokenColor
@@ -51,7 +54,7 @@ class Grid:
                 break
         result = [1, 1, 1, 1]
         continueTest = [1, 1, 1, 1, 1, 1, 1, 1]
-        
+
         for i in range(1, self._tokenWinNumber):
             # test horizontal - on scrute la couleur des jetons a gauche puis a droite
             if x - i >= 0:  # pas de debordement hors de la grille a gauche
@@ -60,14 +63,14 @@ class Grid:
                         result[0] = result[0] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[0] = 0  # interruption de couleur - on arrete de chercher a gauche
-            
+
             if x + i < self._numWidth:  # pas de debordement hors de la grille a droite
                 if continueTest[1] == 1:  # on continue car pas d'interruption de couleur a droite
                     if self._gridList[x + i][y] == tokenColor:  # le jeton est de la bonne couleur
                         result[0] = result[0] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[1] = 0  # interruption de couleur - on arrete de chercher a droite
-           
+
             # test vertical - on scrute la couleur des jetons en bas puis en haut
             if y - i >= 0:  # pas de debordement hors de la grille en bas
                 if continueTest[2] == 1:  # on continue car pas d'interruption de couleur par le bas
@@ -75,47 +78,47 @@ class Grid:
                         result[1] = result[1] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[2] = 0  # interruption de couleur - on arrete de chercher en bas
-           
+
             if y + i < self._numHeight:  # pas de debordement hors de la grille en haut
                 if continueTest[3] == 1:  # on continue car pas d'interruption de couleur par le haut
                     if self._gridList[x][y + i] == tokenColor:  # le jeton est de la bonne couleur
                         result[1] = result[1] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[3] = 0  # interruption de couleur - on arrete de chercher a droite
-            
+
             # test diagonal montante - on scrute vers le bas a gauche et le haut a droite
             if x - i >= 0 and y - i >= 0:  # pas de debordement hors de la grille a gauche et en bas
                 if continueTest[4] == 1:  # on continue car pas d'interruption de couleur par le bas
                     if self._gridList[x - i][y - i] == tokenColor:  # le jeton est de la bonne couleur
-                        result[2] = result[2] + 1 # on increment le nombre de jetons alignes
+                        result[2] = result[2] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[4] = 0  # interruption de couleur - on arrete de chercher a gauche en bas
-            
+
             if x + i < self._numHeight and y + i < self._numHeight:  # pas de debordement hors de la grille a droite et en haut
                 if continueTest[5] == 1:  # on continue car pas d'interruption de couleur par le haut
                     if self._gridList[x + i][y + i] == tokenColor:  # le jeton est de la bonne couleur
-                        result[2] = result[2] + 1 # on increment le nombre de jetons alignes
+                        result[2] = result[2] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[5] = 0  # interruption de couleur - on arrete de chercher a droite en haut
-            
-            #test diagonale descendante - on scrute vers le haut a gauche et le bas a droite
+
+            # test diagonale descendante - on scrute vers le haut a gauche et le bas a droite
             if x - i >= 0 and y + i < self._numHeight:  # pas de debordement hors de la grille a gauche et en haut
                 if continueTest[6] == 1:  # on continue car pas d'interruption de couleur par le bas a gauche
                     if self._gridList[x - i][y + i] == tokenColor:  # le jeton est de la bonne couleur
-                        result[3] = result[3] + 1 # on increment le nombre de jetons alignes
+                        result[3] = result[3] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[6] = 0  # interruption de couleur - on arrete de chercher en haut a gauche
-            
+
             if x + i < self._numHeight and y - i >= 0:  # pas de debordement hors de la grille a droite et en bas
                 if continueTest[7] == 1:  # on continue car pas d'interruption de couleur par le haut a droite
                     if self._gridList[x + i][y - i] == tokenColor:  # le jeton est de la bonne couleur
                         result[3] = result[3] + 1  # on increment le nombre de jetons alignes
                     else:
                         continueTest[7] = 0  # interruption de couleur - on arrete de chercher en bas a gauche
-            
-            if self._sum(continueTest) == 0: #  il n'y a plus de jeton de couleur alignes
+
+            if self._sum(continueTest) == 0:  # il n'y a plus de jeton de couleur alignes
                 break
-            
+
             for j in result:
                 if j == self._tokenWinNumber:  # le nombre de jetons alignes est le bon
                     self._win = 1
@@ -149,94 +152,168 @@ class Grid:
 
 
     def _sum(self, arr):
-        sum=0
+        sum = 0
         for i in arr:
             sum = sum + i
         return sum
 
 
 
-def printGrid(_grid, nbline, nbcol):
-    line = "|"
-    separator = "-"
-    abscisse = "  "
-    for k in range(nbcol):
-        separator = separator + "----"
-        abscisse = abscisse + str(k) + "   "
-    print(separator)
-    for i in range(nbline - 1, -1, -1):
-        for j in range(nbcol):
-            if _grid[j][i] == 0:
-                line = line + "   |"
-            elif _grid[j][i] == 1:
-                line = line + " X |"
-            elif _grid[j][i] == 2:
-                line = line + " O |"
-        print(line)
-        print(separator)
+class Game:
+
+    def __init__(self):
+        self.listOfPlayers = []
+        self.nbcolumn = 10
+        self.nbline = 10
+        self.nbToken = 4
+        self.numberOfPlayer = 2
+
+
+    def printGrid(self, _grid):
         line = "|"
-    print(abscisse)
-    
-    
-    
-# Param par défaut
-nbcolumn = 10 #peut etre defini par input
-nbline = 10 #peut etre defini par input
-power4Grid = Grid(nbline, nbcolumn, 4) # nb lignes, nb colonnes, nb jetons alignés
-listOfPlayers = []
-numberOfPlayer = 2 # deux joueurs
-player = Player("Player1", "Jaune") # écrit couleur dans classe A MODIFIER
-listOfPlayers.append(player)
-player = Player("Player2", "Rouge") # écrit couleur dans classe A MODIFIER
-listOfPlayers.append(player)
+        separator = "-"
+        abscisse = "  "
+        for k in range(self.nbcolumn):
+            separator = separator + "----"
+            abscisse = abscisse + str(k) + "   "
+        print(separator)
+        for i in range(self.nbline - 1, -1, -1):
+            for j in range(self.nbcolumn):
+                if _grid[j][i] == 0:
+                    line = line + "   |"
+                elif _grid[j][i] == 1:
+                    line = line + " X |"
+                elif _grid[j][i] == 2:
+                    line = line + " O |"
+            print(line)
+            print(separator)
+            line = "|"
+        print(abscisse)
 
 
-endOfGame = False  # Booléen pour continuer la partie en fct de si elle est terminée (s'arrête à 1)
-gameturn = 0  # indique le tour de jeu
-
-
-
-
-while endOfGame == False:
-    if gameturn == 0:
-        print ("\n Début de partieeeeeeeeee ! \n")
-        printGrid(power4Grid.getGrid(), nbline, nbcolumn)
-    gameturn = gameturn + 1  # incrémente le tour
-    print("\n**********")
-    print("* tour", gameturn, "*")
-    print("**********")
-
-    for j in range(numberOfPlayer):
-        currentplayer = listOfPlayers[j]  # récupère le joueur actuel (celui qui joue)
-        test_input = False
+    def Init(self):
+        endRequest = False
+        
+        while not endRequest:
+            request = input("Quel type de partie ? Standard (S) ou Modifiée (M) :")
+            
+            while (request not in ("S","M")) :
+                request = input("Quel type de partie ? Standard (S) ou Modifiée (M) :")
                 
-        while test_input == False:
-            column_input = input(currentplayer._name + " choisissez une colonne pour votre jeton (allant de 0 à " + str(nbcolumn-1) + "):")
-           
-            try:
-                val_column_input = int(column_input)
-                if 0 <= val_column_input <= (nbcolumn-1):
-                    if power4Grid.TestEndColumn(val_column_input):
-                        print("La colonne est pleine!")
-                    else:
-                        test_input = True
+            if request == "S":
+                endRequest = True
+                
+            elif request == "M":   
+                while not endRequest:
+                    endRequestCol = False
+                    while not endRequestCol:
+                        _nbcol = input("Nombre de colonnes (entre 5 et 20) :")
                         
-            except ValueError:
-                print("Le numero de colonne n'est pas valide !")
-        power4Grid.Throw(val_column_input, currentplayer.Get_Token())
-        printGrid(power4Grid.getGrid(), nbline, nbcolumn)
-        
-        if power4Grid.TestIfWin():
-            print(currentplayer._name + " gagne la partie!")
-            endOfGame = True
-            break
-        
-        if power4Grid.TestEndOfGame():
-            print("Personne ne gagne")
-            endOfGame = True
-            break
+                        try:
+                            val_nbcol = int(_nbcol)
+                            if 5 <= val_nbcol <= 20:
+                                self.nbcolumn = val_nbcol
+                                endRequestCol = True
+                            else:
+                                print("Le nombre de colonnes n'est pas valide !")
+                                
+                        except ValueError:
+                            print("La saisie n'est pas un nombre !")
+                            
+                    endRequestLine = False
+                    
+                    while not endRequestLine:
+                        _nbLine = input("Nombre de lignes (entre 5 et 20) :")
+                        
+                        try:
+                            val_nbLine = int(_nbLine)
+                            if 5 <= val_nbLine <= 20:
+                                self.nbline = val_nbLine
+                                endRequestLine = True
+                            else:
+                                print("Le nombre de lignes n'est pas valide !")
+                                
+                        except ValueError:
+                            print("La saisie n'est pas un nombre !")
+                            
+                    endRequestToken = False
+                    
+                    while not endRequestToken:
+                        _nbToken = input("Nombre de jetons (entre 3 et 10) :")
+                        
+                        try:
+                            val_nbToken = int(_nbToken)
+                            if 3 <= val_nbToken <= 10:
+                                self.nbToken = val_nbToken
+                                endRequestToken = True
+                            else:
+                                print("Le nombre de jetons n'est pas valide !")
+                                
+                        except ValueError:
+                            print("La saisie n'est pas un nombre !")
+                            
+                    endRequest = True
+                    
+        self.power4Grid = Grid(self.nbline, self.nbcolumn, self.nbToken)  # nb lignes, nb colonnes, nb jetons alignés
+
+
+    def set_Players(self, playerClass):
+        self.listOfPlayers.append(playerClass)
+
+
+    def Play(self):
+        endOfGame = False  # Booléen pour continuer la partie en fct de si elle est terminée (s'arrête à 1)
+        gameturn = 0  # indique le tour de jeu
+        while endOfGame == False:
+            if gameturn == 0:
+                print("\n Début de partieeeeeeeeee ! \n")
+                self.printGrid(self.power4Grid.getGrid())
+            gameturn = gameturn + 1  # incrémente le tour
+            print("\n**********")
+            print("* tour", gameturn, "*")
+            print("**********")
+
+            for j in range(self.numberOfPlayer):
+                currentplayer = self.listOfPlayers[j]  # récupère le joueur actuel (celui qui joue)
+                test_input = False
+
+                while test_input == False:
+                    column_input = input(currentplayer._name + " choisissez une colonne pour votre jeton (allant de 0 à " + str(
+                        self.nbcolumn - 1) + "):")
+
+                    try:
+                        val_column_input = int(column_input)
+                        if 0 <= val_column_input <= (self.nbcolumn - 1):
+                            if self.power4Grid.TestEndColumn(val_column_input):
+                                print("La colonne est pleine!")
+                            else:
+                                test_input = True
+
+                    except ValueError:
+                        print("Le numero de colonne n'est pas valide !")
+                self.power4Grid.Throw(val_column_input, currentplayer.Get_Token())
+                self.printGrid(self.power4Grid.getGrid())
+
+                if self.power4Grid.TestIfWin():
+                    print(currentplayer._name + " gagne la partie!")
+                    endOfGame = True
+                    break
+
+                if self.power4Grid.TestEndOfGame():
+                    print("Personne ne gagne")
+                    endOfGame = True
+                    break
 
 
 
+#main
+gameP4 = Game()
+gameP4.Init()
+player1 = Player()
+player1.Set_Param("Player1", "Jaune")
+gameP4.set_Players(player1)
 
-
+player2 = Player()
+player2.Set_Param("Player2", "Rouge")
+gameP4.set_Players(player2)
+gameP4.Play()
