2c8f7e0e78d0d84c24bf13e85093d0e9ff22f0aa
Clement GABAS <clementgabascg65@gmail.com>
2020-11-06 20:26:28 +0100

    maj player

--- AppClient/Travail/Player/PlayerClass.py
+++ AppClient/Travail/Player/PlayerClass.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 class Player():
 
     def __init__(self, pseudo, id_salle, chef_salle, jeu):
@@ -10,8 +13,15 @@ class Player():
         pass
 
     def ask_api_if_tour(self):
-        pass
+        dataPost = {"id_salle":self.id_salle,
+                    "pseudo_joueur":self.pseudo}
+        res = requests.get(url="http://localhost:9090/home/game/room/play", data=json.dumps(dataPost))
+
+        if res.status_code == 200:
+            return True
+        else:
+            return False
+
 
     def ask_api_if_partie_lancee(self):
         pass
-        
